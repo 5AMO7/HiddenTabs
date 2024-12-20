56ce452b2143ddb0af88562210d8acf5d60395fd
5AMO7 <119872374+5AMO7@users.noreply.github.com>
2024-12-21 01:06:02 +0200

    Select App, Window positions
    Added a select app feature, not finished though.
    Added a Window positions config file, for saving a users config

--- hiddentabs.py
+++ hiddentabs.py
@@ -1,20 +1,62 @@
 from pystray import Icon, Menu, MenuItem
 from PIL import Image
 import sys
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+import darkdetect
+import sv_ttk
 
-def openApp():
-    print("opened")
+def on_select(event):
+    selected_item = comboBox.get()
+    print(selected_item)
 
-def exitApp(icon):
-    icon.stop()
-    sys.exit()
+def select_file():
+    filetypes = [
+        ("application files (*.exe)", ".exe")
+    ]
 
-menu = Menu(
-    MenuItem('Open HiddenTabs', openApp),
-    MenuItem('Exit', exitApp)
-)
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
 
-image = Image.open("resources/icon.png")
+    comboBox.set(filename.split("/")[-1])
 
-icon = Icon("MyApp", image, "HiddenTabs", menu)
-icon.run()
+
+root = tk.Tk()
+
+root.title("HiddenTabs")
+root.geometry("600x400")
+root.wm_iconphoto(True, tk.PhotoImage(file="resources/icon.png"))
+
+selectLabel = ttk.Label(root, text="Select an application:", font=("TkDefaultFont", 16))
+selectLabel.pack(pady=(20, 10))
+
+comboBox = ttk.Combobox(root, state="readonly", values=["Option 1", "Option 2", "Option 3"])
+comboBox.bind("<<ComboboxSelected>>", lambda e: print(comboBox.get()))
+
+button = ttk.Button(root, text="Select a different app", command=select_file)
+
+comboBox.pack()
+button.pack(pady=(10))
+
+sv_ttk.set_theme("dark")
+root.mainloop()
+
+# def openApp():
+#     print("opened")
+
+# def exitApp(icon):
+#     icon.stop()
+#     sys.exit()
+
+# menu = Menu(
+#     MenuItem('Open HiddenTabs', openApp),
+#     MenuItem('Exit', exitApp)
+# )
+
+# image = Image.open("resources/icon.png")
+
+# icon = Icon("MyApp", image, "HiddenTabs", menu)
+# icon.run()
