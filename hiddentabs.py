837c0cdcc2a78f4f5fbf7eeae73ba25ef363f1aa
5AMO7 <119872374+5AMO7@users.noreply.github.com>
2024-12-21 13:43:30 +0200

    Main function
    Basically finished the project
    TODO:
    -Comments
    -Fix a case where a window is closed it throws an error

--- hiddentabs.py
+++ hiddentabs.py
@@ -6,9 +6,16 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 import darkdetect
 import sv_ttk
+import win32gui
+import win32con
+import win32process
+import psutil
+import configparser
+from pynput import mouse
+import threading
 
 def on_select(event):
-    selected_item = comboBox.get()
+    selected_item = selectComboBox.get()
     print(selected_item)
 
 def select_file():
@@ -21,42 +28,181 @@ def select_file():
         initialdir='/',
         filetypes=filetypes)
 
-    comboBox.set(filename.split("/")[-1])
+    selectComboBox.set(filename.split("/")[-1])
+
+def get_hwnd(exe):
+    exe_hwnd = None
+    def callback(hwnd, windows):
+        nonlocal exe_hwnd
+        if exe_hwnd is not None:
+            return
+        
+        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd):
+            pid = win32process.GetWindowThreadProcessId(hwnd)[1]
+            try:
+                process = psutil.Process(pid)
+                process_exe = process.name()
+
+                if process_exe.lower() == exe.lower():
+                    exe_hwnd = hwnd
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                pass
+
+    win32gui.EnumWindows(callback, None)
+    return exe_hwnd
+
+def get_pos_from_hwnd(hwnd):
+    rect = win32gui.GetWindowRect(hwnd)
+    left, top, right, bottom = rect
+    width = right - left
+    height = bottom - top
+
+    return left, top, width, height
+
+def set_open_pos():
+    exe = selectComboBox.get()
+    hwnd = get_hwnd(exe)
+    left, top, width, height = get_pos_from_hwnd(hwnd)
+
+    config = configparser.ConfigParser()
+    config.read('window_positions.ini')
+
+    if str(exe) in config:
+        config[str(exe)]["opened_pos"] = str(left) + "," + str(top) + "," + str(width) + "," + str(height)
+    else:
+        config[str(exe)] = {
+            "opened_pos": str(left) + "," + str(top) + "," + str(width) + "," + str(height),
+            "closed_pos": str(left) + "," + str(top) + "," + str(width) + "," + str(height)
+        }
+
+    with open('window_positions.ini', 'w') as configfile:
+        config.write(configfile)
+
+    print(str(left) + ", " + str(top) + ", " + str(width) + ", " + str(height))
+
+def set_close_pos():
+    exe = selectComboBox.get()
+    hwnd = get_hwnd(exe)
+    left, top, width, height = get_pos_from_hwnd(hwnd)
+
+    config = configparser.ConfigParser()
+    config.read('window_positions.ini')
+
+    if str(exe) in config:
+        config[str(exe)]["closed_pos"] = str(left) + "," + str(top) + "," + str(width) + "," + str(height)
+    else:
+        config[str(exe)] = {
+            "opened_pos": str(left) + "," + str(top) + "," + str(width) + "," + str(height),
+            "closed_pos": str(left) + "," + str(top) + "," + str(width) + "," + str(height)
+        }
+
+    with open('window_positions.ini', 'w') as configfile:
+        config.write(configfile)
+
+    print(str(left) + ", " + str(top) + ", " + str(width) + ", " + str(height))
 
 
-root = tk.Tk()
+def open_window(hwnd):
+    config = configparser.ConfigParser()
+    config.read('window_positions.ini')
+    pid = win32process.GetWindowThreadProcessId(hwnd)[1]
+    exe = psutil.Process(pid).name()
+    
+    pos = config[str(exe)]["opened_pos"].split(",")
+    left, top, width, height = int(pos[0]), int(pos[1]), int(pos[2]), int(pos[3])
+    win32gui.MoveWindow(hwnd, left, top, width, height, True)
 
-root.title("HiddenTabs")
-root.geometry("600x400")
-root.wm_iconphoto(True, tk.PhotoImage(file="resources/icon.png"))
+def close_windows():
+    config = configparser.ConfigParser()
+    config.read('window_positions.ini')
+    
+    for exe in config.sections():
+        hwnd = get_hwnd(exe)
+        pos = config[str(exe)]["closed_pos"].split(",")
+        left, top, width, height = int(pos[0]), int(pos[1]), int(pos[2]), int(pos[3])
+        win32gui.MoveWindow(hwnd, left, top, width, height, True)
 
-selectLabel = ttk.Label(root, text="Select an application:", font=("TkDefaultFont", 16))
-selectLabel.pack(pady=(20, 10))
 
-comboBox = ttk.Combobox(root, state="readonly", values=["Option 1", "Option 2", "Option 3"])
-comboBox.bind("<<ComboboxSelected>>", lambda e: print(comboBox.get()))
+def on_click(x, y, button, pressed):
+    config = configparser.ConfigParser()
+    config.read('window_positions.ini')
 
-button = ttk.Button(root, text="Select a different app", command=select_file)
+    pressedWindow = win32gui.GetForegroundWindow()
+    pid = win32process.GetWindowThreadProcessId(pressedWindow)[1]
+    
+    if pid < 0:
+        close_windows()
+        return
+    
+    activeExe = psutil.Process(pid).name()
 
-comboBox.pack()
-button.pack(pady=(10))
+    if activeExe in config:
+        open_window(pressedWindow)
+    else:
+        close_windows()
 
-sv_ttk.set_theme("dark")
-root.mainloop()
+def start_gui():
+    def set_combobox_apps():
+        config = configparser.ConfigParser()
+        config.read('window_positions.ini')
 
-# def openApp():
-#     print("opened")
+        for section in config.sections():
+            selectComboBox['values'] = (*selectComboBox['values'], section)
 
-# def exitApp(icon):
-#     icon.stop()
-#     sys.exit()
+    root = tk.Tk()
 
-# menu = Menu(
-#     MenuItem('Open HiddenTabs', openApp),
-#     MenuItem('Exit', exitApp)
-# )
+    root.title("HiddenTabs")
+    root.geometry("600x400")
+    root.wm_iconphoto(True, tk.PhotoImage(file="resources/icon.png"))
 
-# image = Image.open("resources/icon.png")
+    selectLabel = ttk.Label(root, text="Select an application:", font=("TkDefaultFont", 16))
+    selectLabel.pack(pady=(20, 10))
 
-# icon = Icon("MyApp", image, "HiddenTabs", menu)
-# icon.run()
+    selectComboBox = ttk.Combobox(root, state="readonly")
+    selectComboBox.bind("<<ComboboxSelected>>", lambda e: print(selectComboBox.get()))
+    selectComboBox.pack()
+    set_combobox_apps()
+
+    selectButton = ttk.Button(root, text="Select a different app", command=select_file)
+    selectButton.pack(pady=(10))
+
+    setOpenButton = ttk.Button(root, text="Set Open Position", command=set_open_pos)
+    setOpenButton.pack(pady=(10))
+    setCloseButton = ttk.Button(root, text="Set Close Position", command=set_close_pos)
+    setCloseButton.pack(pady=(10))
+
+    sv_ttk.set_theme("dark")
+
+    root.mainloop()
+
+def start_tray():
+    def openApp():
+        start_gui()
+
+    def exitApp(icon):
+        icon.stop()
+        sys.exit()
+
+    menu = Menu(
+        MenuItem('Open HiddenTabs', openApp),
+        MenuItem('Exit', exitApp)
+    )
+
+    image = Image.open("resources/icon.png")
+
+    icon = Icon("MyApp", image, "HiddenTabs", menu)
+
+    icon.run()
+
+def start_listener():
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    listener_thread = threading.Thread(target=start_listener, daemon=True)
+    gui_thread = threading.Thread(target=start_gui, daemon=True)
+
+    listener_thread.start()
+    gui_thread.start()
+    start_tray()
+
